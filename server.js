39a34d7a7c766d006f00081c594e1e8fbd7924e1
Alfredo Lara <alaradotor@gmail.com>
2020-08-14 12:00:48 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales', function(err) {});
+app.set('view engine', 'hbs');
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Alfredo'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+
+});
+
+
+// app.get('/', (req, res) => {
+//     // res.send('Hello World')
+//     let salida = {
+//         nombre: 'ald',
+//         edad: 32,
+//         url: req.url
+//     }
+//     res.send(salida);
+// });
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en elpuerto ${port}`);
+});
